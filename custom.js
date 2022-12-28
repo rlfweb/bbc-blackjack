dbeaede501f43ccce43245fdb2d39c55b00499f0
REBECCA FRENCH <rlfweb8@gmail.com>
2022-12-28 14:06:28 +0000

    pseudocode for shuffle and deal

---
+++ custom.js
@@ -0,0 +1,21 @@
+// pseudocode
+
+// onclick SHUFFLE 
+
+// have an array with 52 cards 
+
+// onclick DEAL
+
+// use Math.random to choose a card and render that card in the DOM within player 1's hand
+// for that card change inDeck to false
+
+// repeat for card 2 to player 2's hand
+
+// repeat for card 3 to player 1's hand
+
+// repeat for card 4 to player 2's hand
+
+// total each player's hand 
+
+// game is ready to play
+
